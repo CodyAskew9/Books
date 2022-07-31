dd956473faaf81ec26b8ae06346fd9a692438d48
cody askew <codyaskew9@gmail.com>
2022-07-30 21:12:25 -0600

    working on routes

---
+++ client/src/component/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div >
+        <ul className='navLinks'>
+            <li>Home</li>
+            <li>Profiles</li>
+            <li>CheckOut</li>
+            <li>Return</li>
+            <li>Login</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
